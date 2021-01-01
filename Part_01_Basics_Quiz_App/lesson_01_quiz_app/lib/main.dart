a79a7165c6e55f3540e847a43d2e45dfe37d8d40
Minh Tin Tang <minhtintang@gmail.com>
2021-01-01 15:00:01 +0700

    Finished  Personal Expenses App

--- Part_01_Basics_Quiz_App/lesson_01_quiz_app/lib/main.dart
+++ Part_01_Basics_Quiz_App/lesson_01_quiz_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 // import 'package:lesson_01_quiz_app/answer.dart';
 // import 'package:lesson_01_quiz_app/question.dart';
 
-import 'answer.dart';
 import 'quiz.dart';
 import 'result.dart';
 
@@ -25,8 +24,8 @@ class _MyAppState extends State {
       'questionText': 'What\'s your favorite color?',
       'answers': [
         {'text': 'Black', 'score': 15},
-        {'text': 'Red', 'score': 10}, 
-        {'text': 'Green', 'score': 5}, 
+        {'text': 'Red', 'score': 10},
+        {'text': 'Green', 'score': 5},
         {'text': 'White', 'score': 10}
       ]
     },
@@ -34,8 +33,8 @@ class _MyAppState extends State {
       'questionText': 'What\'s your favorite animal?',
       'answers': [
         {'text': 'Rabbit', 'score': 5},
-        {'text': 'Snake', 'score': 10}, 
-        {'text': 'Elephant', 'score': 25}, 
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Elephant', 'score': 25},
         {'text': 'Loin', 'score': 5}
       ]
     },
@@ -43,8 +42,8 @@ class _MyAppState extends State {
       'questionText': 'What\'s your name?',
       'answers': [
         {'text': 'Tin', 'score': 15},
-        {'text': 'Nam', 'score': 20}, 
-        {'text': 'Vinh', 'score': 15}, 
+        {'text': 'Nam', 'score': 20},
+        {'text': 'Vinh', 'score': 15},
         {'text': 'Thinh', 'score': 10}
       ]
     },
@@ -71,18 +70,18 @@ class _MyAppState extends State {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: _questionIndex < _questions.length 
-        ? Quiz(
-          answerQuestion: _answerQuestion,
-          questionIndex: _questionIndex,
-          questions: _questions,
-        ) : Result(_totalScore, _resetQuiz),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
---
+++ Part_02_Widgets_Styling_Adding_Logic/PersonalExpensesApp/lib/widgets/new_transaction.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+
+    final String titleEntered = _titleController.text;
+    final double amountEntered = double.parse(_amountController.text);
+
+    if (titleEntered.isEmpty || amountEntered <= 0 || _selectedDate == null) {
+      return;
+    }
+    widget.addTx(
+      titleEntered,
+      amountEntered,
+      _selectedDate,
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitData(),
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _presentDatePicker,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
+              child: Text('Add Transaction'),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
